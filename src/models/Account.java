22a91df673caa23ff17031ebedf18345d79dc09c
Volodymyr Shabanov <volodymyrshw@gmail.com>
2023-11-28 19:10:58 +0100

    merge

--- src/models/Account.java
+++ src/models/Account.java
@@ -7,24 +7,8 @@ public class Account {
     private String userEmail;
     private double balance;
     private Currency currency;
+    private static int counter;
 
-<<<<<<< HEAD
-    public Account(int id, User user, Currency currency) {
-        this.id = id;
-        this.user = user;
-        this.currency = currency;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", user=" + user +
-                ", balance=" + balance +
-                ", currency=" + currency +
-                '}';
-=======
     public Account(String userID, double balance, Currency currency) {
         this.id = counter++;
         this.userEmail = userID;
@@ -39,7 +23,7 @@ public class Account {
                 currency.toString(),
                 balance
         );
->>>>>>> f7e6ef14b60aabb5f9af5df86f1170abfb04740f
+
     }
 
     public long getId() {
@@ -50,13 +34,6 @@ public class Account {
         return userEmail;
     }
 
-<<<<<<< HEAD
-=======
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
->>>>>>> f7e6ef14b60aabb5f9af5df86f1170abfb04740f
     public double getBalance() {
         return balance;
     }
